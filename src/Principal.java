98b7125610f8a84da86be9ad099ce04a99eb8738
XavRuiz <javier.ruizxl@gmail.com>
2017-06-27 19:53:45 -0600

    Creacion de Case Sin Balon

--- src/Principal.java
+++ src/Principal.java
@@ -2,4 +2,7 @@
  * Created by alumno on 27/06/2017.
  */
 public class Principal {
+
+
+
 }
---
+++ src/SinBalon.java
@@ -0,0 +1,28 @@
+
+/**
+ * Created by alumno on 27/06/2017.
+ */
+public class SinBalon {
+
+    List <String> listSinBalon;
+    String [] dep = {"Boxeo","Natacion","Karate","Tiro al arco"};
+
+    public SinBalon() {
+
+        listSinBalon = new ArrayList<String>();
+        for (String depo : dep) {
+
+        }
+    }
+
+    public List<String> getListSinBalon(){
+        return listSinBalon;
+
+    }
+    
+    public void agregarDeportesSinBalon (String dep) {
+
+
+
+    }
+}
